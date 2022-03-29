fb276e5c33c7def7e1ca29cb08ffd327bae5b451
mcguirepr89 <mcguirepr89@gmail.com>
2022-03-29 14:11:26 -0400

    adding templates directory and bashrc

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 #!/home/pi/BirdNET-Analyzer-Pi/birdnet/bin/python3
+#!/home/pi/BirdNET-Analyzer-Pi/birdnet/bin/python3
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
