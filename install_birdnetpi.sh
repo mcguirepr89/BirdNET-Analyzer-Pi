c7a85d727ef56edcc6d50c9bc8d84c7229bf4f2f
mcguirepr89 <mcguirepr89@gmail.com>
2022-04-01 13:23:00 -0400

    moving sourcing to after auto-detect settings

--- install_birdnetpi.sh
+++ install_birdnetpi.sh
@@ -15,15 +15,24 @@ export configpy=$configpy
 
 #caddy_url="https://dl.cloudsmith.io/public/caddy/stable/setup.deb.sh"
 dependencies=(git python3-dev python3-venv python3-pip ffmpeg sqlite3 alsa-utils pulseaudio)
+
 install_birdnet() {
-  git clone git@github.com:mcguirepr89/BirdNET-Analyzer-Pi.git ${my_dir}
-  source <(grep -ve '^$' -e '^#' <(sed 's/ //g' $configpy | sed '/Getandset/q'))
+  git clone git@github.com:mcguirepr89/BirdNET-Analyzer-Pi.git /home/pi/BirdNET-Analyzer-Pi
   cd ~/BirdNET-Analyzer-Pi
   python3 -m venv birdnet
   source ./birdnet/bin/activate
   pip3 install --upgrade pip
   pip3 install librosa tflite-runtime
   deactivate
+}
+
+auto-detect_settings() {
+  LATITUDE="LATITUDE = $(curl -s4 ifconfig.co/json | awk '/lat/ {print $2}' | tr -d ',')"
+  LONGITUDE="LONGITUDE = $(curl -s4 ifconfig.co/json | awk '/lon/ {print $2}' | tr -d ',')"
+  sed -i "s/LATITUDE = -1/$LATITUDE/" ${configpy}
+  sed -i "s/LONGITUDE = -1/$LONGITUDE/" ${configpy}
+  
+  source <(grep -ve '^$' -e '^#' <(sed 's/ //g' $configpy | sed '/Getandset/q'))
 }
 
 install_recording_service() {
@@ -69,6 +78,9 @@ sudo apt -y install --no-install-recommends ${dependencies[@]}
 
 echo "Installing BirdNET-Analyzer"
 install_birdnet
+
+echo "Auto-detecting some settings"
+auto-detect_settings
 
 echo "Installing Recording Service"
 install_recording_service
