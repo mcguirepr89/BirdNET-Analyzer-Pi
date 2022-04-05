b7277e7596adee8dfec044b4f3328a04447f447a
mcguirepr89 <mcguirepr89@gmail.com>
2022-04-05 14:52:05 -0400

    new .gitignore, verbose analyze.py, services log in the BirdNET-Analyzer-Pi directory

--- install_birdnetpi.sh
+++ install_birdnetpi.sh
@@ -16,8 +16,8 @@ export configpy=$configpy
 caddy_url="https://dl.cloudsmith.io/public/caddy/stable/setup.deb.sh"
 dependencies=(git python3-dev python3-venv python3-pip ffmpeg caddy sqlite3 alsa-utils pulseaudio)
 install_birdnet() {
-  git clone git@github.com:mcguirepr89/BirdNET-Analyzer-Pi.git /home/pi/BirdNET-Analyzer-Pi
-  cd ~/BirdNET-Analyzer-Pi
+  git clone git@github.com:mcguirepr89/BirdNET-Analyzer-Pi.git $my_dir
+  cd $my_dir
   python3 -m venv birdnet
   source ./birdnet/bin/activate
   pip3 install --upgrade pip
@@ -40,7 +40,7 @@ auto-detect_settings() {
 }
 
 install_birdnet_analysis() {
-  cat << EOF > /home/pi/BirdNET-Analyzer-Pi/templates/birdnet_analysis.service
+  cat << EOF > $my_dir/templates/birdnet_analysis.service
 [Unit]
 Description=BirdNET Analysis
 [Service]
@@ -48,17 +48,19 @@ Restart=always
 Type=simple
 RestartSec=2
 User=${USER}
-ExecStart=/home/pi/BirdNET-Analyzer-Pi/birdnet_analysis.sh
+ExecStart=$my_dir/birdnet_analysis.sh
+StandardOutput=append:$my_dir/birdnet_analysis.log
+StandardError=append:$my_dir/birdnet_analysis.log
 [Install]
 WantedBy=multi-user.target
 EOF
-  sudo ln -sf /home/pi/BirdNET-Analyzer-Pi/templates/birdnet_analysis.service /usr/lib/systemd/system
+  sudo ln -sf $my_dir/templates/birdnet_analysis.service /usr/lib/systemd/system
   sudo systemctl enable birdnet_analysis.service
 }
 
 install_recording_service() {
   echo "Installing birdnet_recording.service"
-  cat << EOF > /home/pi/BirdNET-Analyzer-Pi/templates/birdnet_recording.service
+  cat << EOF > $my_dir/templates/birdnet_recording.service
 [Unit]
 Description=BirdNET Recording
 [Service]
@@ -67,11 +69,13 @@ Restart=always
 Type=simple
 RestartSec=3
 User=${USER}
-ExecStart=/home/pi/BirdNET-Analyzer-Pi/birdnet_recording.sh
+ExecStart=$my_dir/birdnet_recording.sh
+StandardOutput=append:$my_dir/birdnet_recording.log
+StandardError=append:$my_dir/birdnet_recording.log
 [Install]
 WantedBy=multi-user.target
 EOF
-  sudo ln -sf /home/pi/BirdNET-Analyzer-Pi/templates/birdnet_recording.service /usr/lib/systemd/system
+  sudo ln -sf $my_dir/templates/birdnet_recording.service /usr/lib/systemd/system
   sudo systemctl enable birdnet_recording.service
 }
 
