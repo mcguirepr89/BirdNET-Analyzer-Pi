8f0a7df2d56799c7af252654fcc5b83b3a83e4bf
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-04-14 15:34:41 -0400

    Update install_birdnetpi.sh

--- install_birdnetpi.sh
+++ install_birdnetpi.sh
@@ -16,7 +16,7 @@ export configpy=$configpy
 caddy_url="https://dl.cloudsmith.io/public/caddy/stable/setup.deb.sh"
 dependencies=(git python3-dev python3-venv python3-pip ffmpeg caddy sqlite3 alsa-utils pulseaudio)
 install_birdnet() {
-  git clone git@github.com:mcguirepr89/BirdNET-Analyzer-Pi.git $my_dir
+  git clone https://github.com/mcguirepr89/BirdNET-Analyzer-Pi.git $my_dir
   cd $my_dir
   python3 -m venv birdnet
   source ./birdnet/bin/activate
