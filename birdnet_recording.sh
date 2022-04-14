619262e768390cf85590f6eb02ba3dfc3bc0609e
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2022-04-14 16:11:49 -0400

    Update birdnet_recording.sh

--- birdnet_recording.sh
+++ birdnet_recording.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -x
-my_dir=/home/pi/BirdNET-Analyzer-Pi
-configpy=${my_dir}/config.py
+my_dir=$HOME/BirdNET-Analyzer-Pi
+configpy=$my_dir/config.py
 source <(grep -ve '^$' -e '^#'  <(sed 's/ //g' $configpy | sed '/Getandset/q'))
-RECS_DIR=~/BirdNET-Analyzer-Pi/$RECS_DIR
+RECS_DIR=$HOME/BirdNET-Analyzer-Pi/$RECS_DIR
 
 [ -z $RECORDING_LENGTH ] && RECORDING_LENGTH=15
 [ -z $CHANNELS ] && CHANNELS=2
@@ -14,6 +14,6 @@ if pgrep arecord &> /dev/null ;then
   echo "Recording"
 else
   arecord -f S16_LE -c${CHANNELS} -r48000 -t wav --max-file-time \
-    ${RECORDING_LENGTH} --use-strftime \
-    ${RECS_DIR}/%F_%H:%M:%S.wav
+    $RECORDING_LENGTH --use-strftime \
+    $RECS_DIR/%F_%H:%M:%S.wav
 fi
